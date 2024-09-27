43e04dc4e625994ce827085d9338c5480483ab0c
TRAHIM86 <trahim1986@gmail.com>
2024-09-27 18:49:17 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !==0) {return false}
+  
+  let pairForBrac = Object.fromEntries(bracketsConfig);
+
+  let pairForBracReverse = {};
+
+  Object.entries(pairForBrac).forEach(([key, value]) => {
+  pairForBracReverse[value] = key;
+})
+  
+  let typesOpenBrac = Object.values(pairForBracReverse);
+  
+  let bracStack = [];
+
+    for (let i = 0; i < str.length; i += 1) {
+
+    if (str[i] === bracStack[bracStack.length - 1] && str[i] === pairForBrac[str[i]]) {
+      bracStack.pop();
+    } else  if (typesOpenBrac.includes(str[i])) {
+      bracStack.push(str[i]);
+      //console.log(bracStack)
+    } else {
+      if (bracStack.length === 0) {
+        return false;
+      }
+      if (pairForBracReverse[str[i]] === bracStack[bracStack.length - 1]) {
+        
+        bracStack.pop();
+      } else {
+        return false;
+      }
+    }
+  } return bracStack.length === 0;
 }
